439bb6d6071789249e123eab72f238cbb4ce8763
vivekchamp84@gmail.com <sibikumark1602@gmail.com>
2024-10-10 08:34:54 +0530

    feat: img preview in pe

---
+++ healthcare_customization/healthcare_customization/customization/patient_encounter/patient_encounter.js
@@ -0,0 +1,40 @@
+// Copyright (c) 2024, Sibikumar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Patient Encounter", {
+	refresh(frm) {
+        if(!frm.doc.custom_from_date)frm.set_value("custom_from_date", frappe.datetime.get_today())
+        if(!frm.doc.custom_to_date)frm.set_value("custom_to_date", frappe.datetime.get_today())
+        pre_image(frm)
+	},
+    custom_from_date(frm){
+        pre_image(frm)
+    },
+    custom_to_date(frm){
+        pre_image(frm)
+    },
+});
+
+function pre_image(frm){
+    if(!frm.doc.custom_from_date){
+        frm.set_df_property("custom_from_html", "options", "<html><h2>Select From Date...</h2><html>")
+    }
+    if(!frm.doc.custom_to_date){
+        frm.set_df_property("custom_to_html", "options", "<html><h2>Select To Date...</h2><html>")
+    }
+    frm.call({
+        method: "healthcare_customization.healthcare_customization.customization.patient_encounter.patient_encounter.preview_image",
+        args: {
+            self: frm.doc
+        },
+        freeze: true,
+        freeze_message: __("Reposting..."),
+        callback: (r) => {
+            if (r.message) {
+                frm.set_df_property("custom_from_html", "options", r.message[0])
+                frm.set_df_property("custom_to_html", "options", r.message[1])
+            }
+        },
+    });
+    
+}
---
+++ healthcare_customization/healthcare_customization/customization/patient_encounter/patient_encounter.py
@@ -0,0 +1,109 @@
+import frappe
+from frappe import _
+import json
+
+@frappe.whitelist()
+def preview_image(self):
+    self = json.loads(self)
+    if not self.get('patient'):
+        return "", ""
+    if self.get('custom_from_date') and self.get('custom_to_date'):
+        if self.get('custom_from_date') > self.get('custom_to_date'):
+            frappe.throw(_("From Date is Greater then To Date"))
+    
+    filter = {
+        "parenttype": "Patient Encounter"
+    }
+    get_pe = frappe.get_all("Patient Encounter", filters={"patient": self.get('patient')}, pluck="name")
+    filter['parent'] = ["in", get_pe]
+    filter["date"] = self.get('custom_from_date')
+    from_get_img = frappe.get_all("Patient Health Image", filters=filter, fields=["image_file", "date"], order_by="date asc")
+    filter["date"] = self.get('custom_to_date')
+    to_get_img = frappe.get_all("Patient Health Image", filters=filter, fields=["image_file", "date"], order_by="date asc")
+    html = '''
+        <html>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f8f9fa;
+                    margin: 0;
+                    padding: 20px;
+                }
+
+                .gallery-container {
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: center;
+                    gap: 20px;
+                }
+
+                div.gallery {
+                    margin: 5px;
+                    border: 1px solid #ccc;
+                    width: 220px;
+                    border-radius: 8px;
+                    overflow: hidden;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                    transition: transform 0.3s ease, box-shadow 0.3s ease;
+                }
+
+                div.gallery:hover {
+                    transform: translateY(-5px);
+                    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+                }
+
+                div.gallery img {
+                    width: 100%;
+                    height: auto;
+                    border-bottom: 1px solid #ddd;
+                }
+
+                div.desc {
+                    padding: 15px;
+                    text-align: center;
+                    background-color: #fff;
+                }
+
+                div.desc .date {
+                    font-size: 14px;
+                    color: #555;
+                }
+
+                div.desc:hover {
+                    color: #007bff;
+                }
+            </style>
+            <body>
+                <div class="gallery-container">
+    '''
+    from_html = to_html = html
+    for i in from_get_img:
+        from_html += f'''
+                    <div class="gallery">
+                        <a target="_blank" href="{i.image_file}">
+                            <img src="{i.image_file}" alt="No Image" width="600" height="400">
+                        </a>
+                        <div class="desc">
+                            <p>Date: <span class="date">{i.date}</span></p>
+                        </div>
+                    </div>
+                '''
+    for i in to_get_img:
+        to_html += f'''
+                    <div class="gallery">
+                        <a target="_blank" href="{i.image_file}">
+                            <img src="{i.image_file}" alt="No Image" width="600" height="400">
+                        </a>
+                        <div class="desc">
+                            <p>Date: <span class="date">{i.date}</span></p>
+                        </div>
+                    </div>
+                '''
+    end_html = '''
+                </div>
+            </body>
+        </html>
+    '''
+
+    return from_html+end_html, to_html+end_html
+
